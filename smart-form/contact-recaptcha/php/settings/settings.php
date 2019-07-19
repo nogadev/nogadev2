0b8c34cd05cb09e2f6aaf5b6fc7a8eff08d669eb
nogadev <49666629+nogadev@users.noreply.github.com>
2019-07-19 10:02:42 -0300

    settings smart-form

--- smart-form/contact-recaptcha/php/settings/settings.php
+++ smart-form/contact-recaptcha/php/settings/settings.php
@@ -14,14 +14,14 @@
 	 * For more than one email go to smartprocess.php then
 	 * Add addresses to the recipients section
 	------------------------------------------------------ */
-	$receiver_email = "example@domain.com";
+	$receiver_email = "fer.nogara@gmail.com";
 	
 	/* If you want to redirect to another page after sending the form
 	 * Change the $redirectForm option below from (false) to (true)
 	 * Then add your redirect page URL replace - http://example.com/thankyou.php
 	----------------------------------------------------------------------------- */	
 	$redirectForm = false;
-	$redirectForm_url = "http://example.com/thankyou.php";
+	$redirectForm_url = "www.nogadev.com/index.html";
 	
 	/* Powered BY
 	 * You will use both your website NAME and URL
