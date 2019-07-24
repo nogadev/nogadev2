3c4c25533becc0db325363fbed1e86e8f28306c5
DanielaLGarro <53092853+DanielaLGarro@users.noreply.github.com>
2019-07-24 18:32:27 -0300

    Formulario
    Se agrego boton de redes sociales,  boton de contactar, formulario funcionando, falta captcha (verificacion). Cambios en algunos estilos.

---
+++ envio.php
@@ -0,0 +1,41 @@
+<?php
+
+$nombre =   isset( $_POST['name'] ) ? $_POST['name'] : '';
+$asunto =   isset( $_POST['asunto'] ) ? $_POST['asunto'] : '';
+$email  =   isset( $_POST['email'] ) ? $_POST['email'] : '';
+$mensaje =  isset( $_POST['mensaje'] ) ? $_POST['mensaje'] : '';
+$contenido = '
+						<html>
+						<head>
+							<title>NOGADEV</title>
+						</head>
+						<body>
+							 <h1>Haz recibido un mensaje através de la página NOGADEV</h1>
+							 <h3>'.$nombre. ' te ha enviado el siguiente mensaje:</h3>
+							 <p>'.$mensaje. ' <p>
+
+							 <br><br><h3> Su email para que lo contactes es: '.$email.'</h3>
+							 <hr>
+							 <p> Enviado el' . date ('d/m/Y');
+						
+					'</body>
+						</html>';
+
+
+// Configuración de los encabezados
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+// Enviar correo
+$envio = mail('danielalilianagarro@gmail.com', $asunto, $contenido, $headers);
+
+
+if($envio) {
+$miresultado = '<h4>El correo ha sido enviado! Gracias por ponerse en contacto con nosotros.</h4>';
+} else{
+
+$miresultado = '<h4>No se envío el correo.</h4>';
+
+}
+
+echo $miresultado;
